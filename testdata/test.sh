9afa383b2af4afb1bfb31158eab5b3613952cc17
magru <magru@08c137f3-715a-4e21-9c70-c0b73baecca8>
2011-03-10 17:17:28 +0000

    integrationtest for flac-sync
    git-svn-id: https://www.linuxalert.org/devcenter/svn/flac-utils/trunk@11 08c137f3-715a-4e21-9c70-c0b73baecca8

---
+++ testdata/test.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+
+prepare() {
+    cp -Ra src test-src
+    cp -Ra dst test-dst
+}
+
+
+clean() {
+   rm -rf test-src test-dst
+   echo
+}
+
+
+result() {
+    echo
+    echo Evaluating test:
+    testOK=true
+    for testfile in `find test-src -type f ! -path "*/.svn/*"`
+    do
+        expectfile=`echo $testfile | sed "s/test-src/expected-result\/$1-src/"`
+        ../tools/flac-diff $testfile $expectfile
+        if [ $? -ne 0 ] ; then
+            testOK=false
+        fi
+    done
+    if $testOK ; then
+	echo -e "\tSrc OK"
+    else 
+	echo -e "\tERROR in src"
+    fi
+
+    testOK=true
+    for testfile in `find test-dst -type f ! -path "*/.svn/*"`
+    do
+        expectfile=`echo $testfile | sed "s/test-dst/expected-result\/$1-dst/"`
+        ../tools/flac-diff $testfile $expectfile
+        if [ $? -ne 0 ] ; then
+            testOK=false
+        fi
+    done
+    if $testOK ; then
+	echo -e "\tDst OK"
+    else 
+	echo -e "\tERROR in dst"
+    fi
+}
+
+testNoArgs() {
+    echo Test No Arguments
+    echo =================
+    prepare
+    ../tools/flac-sync -v test-src test-dst
+    result noargs
+    clean
+}
+
+testCopy() {
+    echo Test Copy
+    echo =========
+    prepare
+    ../tools/flac-sync -v -c test-src test-dst
+    result copy
+    clean
+}
+
+testCopyTime() {
+    echo Test Copy Time
+    echo ==============
+    prepare
+    ../tools/flac-sync -v -c -t test-src test-dst
+    result copy-time
+    clean
+}
+
+testCopyTimeBoth() {
+    echo Test Copy Time Both
+    echo ===================
+    prepare
+    ../tools/flac-sync -v -c -t -b test-src test-dst
+    result copy-time-both
+    clean
+}
+
+testBoth() {
+    echo Test Both
+    echo =========
+    prepare
+    ../tools/flac-sync -v -b test-src test-dst
+    result both
+    clean
+}
+
+
+testOverwrite() {
+    echo Test Overwrite
+    echo ==============
+    prepare
+    ../tools/flac-sync -v -o test-src test-dst
+    result overwrite
+    clean
+}
+
+testTime() {
+    echo Test Time
+    echo =========
+    prepare
+    ../tools/flac-sync -v -t test-src test-dst
+    result time
+    clean
+}
+
+
+testNoArgs
+testCopy
+testCopyTime
+testCopyTimeBoth
+testBoth
+testOverwrite
+testTime
